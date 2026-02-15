ccd2259f9a79ed03b8715861eb20995ddf6a2f60
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-15 16:24:16 -0300

    Rotation and Skale

--- plane-game/Scenes/Ocean/ocean.gd
+++ plane-game/Scenes/Ocean/ocean.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var plane: Sprite2D = $Carrier/Plane
+@onready var plane: Sprite2D = %Plane
 @onready var heli: Sprite2D = $Heli
 @onready var eating_sound: AudioStreamPlayer = $EatingSound
 
@@ -13,9 +13,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# this method allow i control the position x of the plane
-	plane.move_local_x(100.0 * delta)
-	heli.move_local_y(100.0 * delta)
-	heli.rotate(1.5 * delta)
+	plane.position.x += 60.0 * delta
+	heli.position.x += 60.0 * delta
+	#heli.move_local_y(100.0 * delta)
+	#heli.rotate(1.5 * delta)
 	
 	if Input.is_action_pressed("ui_left"):
 		plane.rotate(-1.5 * delta)
