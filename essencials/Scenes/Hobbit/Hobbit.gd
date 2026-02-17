dd192ed0117222d8eedb0d25b2ac63399077a309
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-17 16:44:40 -0300

    Signals PT 1

--- essencials/Scenes/Hobbit/Hobbit.gd
+++ essencials/Scenes/Hobbit/Hobbit.gd
@@ -4,7 +4,12 @@ class_name Hobbit
 extends Node2D
 
 
+@export  var hide_time: float = 2.0
+
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var hide_timer: Timer = $HideTimer
+
 
 
 var p_frames: int = 0
@@ -20,18 +25,24 @@ func _enter_tree() -> void:
 
 
 func _exit_tree() -> void:
-	print("_exit_tree: %s %s" % [get_instance_id(), name])
-
+	#print("_exit_tree: %s %s" % [get_instance_id(), name])
+	pass
 
 func _ready() -> void:
-	print("_ready: %s %s" % [get_instance_id(), name])
-
-
+	#print("_ready: %s %s" % [get_instance_id(), name])
+	hide_timer.start(hide_time)
+	pass
+	
+	
 func _process(delta: float) -> void:
-	p_frames += 1
-	print("_process %d" % p_frames)
+	#p_frames += 1
+	pass
 
 
 func _physics_process(delta: float) -> void:
-	ph_frames += 1
-	print("_physics_process %d" % ph_frames)
+	#ph_frames += 1
+	pass
+
+
+func _on_timer_timeout() -> void:
+	hide()
--- essencials/Scenes/Wizard/Wizard.gd
+++ essencials/Scenes/Wizard/Wizard.gd
@@ -4,6 +4,21 @@ class_name Wizard
 extends Node2D
 
 
+@onready var reveal_timer: Timer = $RevealTimer
+
+
+#var _elapsed: float = 0.0
+
+
+func _ready() -> void:
+	hide()
+
+
 func _process(delta: float) -> void:
+	#_elapsed += delta
 	pass
 	
+
+
+func _on_reveal_timer_timeout() -> void:
+	show()
